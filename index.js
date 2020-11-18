05f0ab0df6921d2616758ff7dfd9d1d187a1b0c2
iceream <1598474045@qq.com>
2020-11-19 01:36:34 +0800

    n

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
 (function (doc) {
+
     let htmlEl = doc.querySelector("html");
+    let wrapperEl = doc.querySelector("#wrapper");
 
-    // let leftBox = doc.querySelector(".left");
-
-    // 页面加载完成 以屏幕宽度设置根元素字体大小 
     window.onload = function () {
-        resetHtmlFontSize();
+        reset ();
+        wrapperEl.style.backgroundColor = "#fff";
     }
 
-    // 监听屏幕改变 
     window.onresize = function () {
-        resetHtmlFontSize();
-    };
+        reset ();
+    }
 
-    function resetHtmlFontSize() {
-        let devicewidth = doc.documentElement.clientWidth;
+    function reset () {
+        let bodyWidth = doc.body.clientWidth;
+        let bodyHeight = doc.body.clientHeight;
+        resetWH(bodyWidth, bodyHeight);
 
-        if (devicewidth < 2400 && devicewidth > 960) {
-            htmlEl.style.fontSize = devicewidth / 80 + "px";
-        } else if (devicewidth < 960) {
-            // 12 * 80 = 960
-            htmlEl.style.fontSize = 12 + "px";
+        let wrapperEl = doc.querySelector("#wrapper");
+        let wrapperWidth = wrapperEl.clientWidth;
+        resetHtmlFontSize(wrapperWidth)
+    }
+
+    function resetWH(bodyWidth, bodyHeight) {
+        // console.log(screenWidth,screenHeight);
+        if (bodyWidth < 768) {
+            wrapperEl.style.width = 100 + "vw";
+            wrapperEl.style.height = 100 + "vh";
+            return;
+        };
+        // screenWidth / screenHeight < 16 / 9
+        if (bodyWidth / bodyHeight <= 16 / 9) {
+            wrapperEl.style.width = bodyWidth + "px";
+            wrapperEl.style.height = bodyWidth / 16 * 9 + "px";
         } else {
-            // 30 * 80 = 2400
-            htmlEl.style.fontSize = 30 + "px";
+            wrapperEl.style.height = bodyHeight + "px";
+            wrapperEl.style.width = bodyHeight * 16 / 9 + "px";
         }
     }
+
+    function resetHtmlFontSize(wrapperWidth) {
+        // console.log(wrapperWidth);
+        if (wrapperWidth < 960) {
+            htmlEl.style.fontSize = 12 + "px";
+        } else if (wrapperWidth > 960  && wrapperWidth < 3200) {
+            htmlEl.style.fontSize = wrapperWidth / 80 + "px";
+        } else {
+            htmlEl.style.fontSize = 40 + "px";
+        }
+    }
+
 })(document);
+
+
 
